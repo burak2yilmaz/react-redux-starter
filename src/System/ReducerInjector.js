99d376c5d2d98f32dc04859671679a4835646c2a
burak2yilmaz <39453705+burak2yilmaz@users.noreply.github.com>
2019-10-17 16:02:29 +0300

    Redux Router Update

---
+++ src/System/ReducerInjector.js
@@ -0,0 +1,34 @@
+export default function reducerInjector(parts) {
+    let stateData = {};
+
+    parts.map(item => {
+        if (item.store) {
+            this[item.subscriber] = item.store.subscribe(() => {
+                reducersInitialize.bind(this)(item.reducers, item.store, item.part, true);
+            });
+            stateData[item.part] = reducersInitialize.bind(this)(item.reducers, item.store, item.part, false);
+        }
+    });
+
+    this.state = stateData;
+}
+
+function reducersInitialize(reducers = [], store, part, isMounted = true) {
+    let storeItems = store.getState(),
+        storageItems = {};
+
+    for (let i = 0; i < reducers.length; i++) {
+        storageItems = {
+            ...storageItems
+        };
+        storageItems[reducers[i]] = storeItems[reducers[i]];
+    }
+
+    if (isMounted) {
+        let setData = {};
+        setData[part] = storageItems;
+        this.setState(setData);
+    }
+    else
+        return storageItems;
+}
